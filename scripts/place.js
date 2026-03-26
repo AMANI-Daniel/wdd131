3570ecb828f1e804d3604da0522b20806fb0f685
AMANI-Daniel <adaniel8@byupathway.edu>
2026-03-26 23:52:06 +0200

    I have used to pseudo-element for the weather image.

--- scripts/place.js
+++ scripts/place.js
@@ -24,4 +24,3 @@ windChill.innerHTML = `${calculateWindChill(temperatureValue, windValue)}°C`;
 function calculateWindChill(temperatureValue, windValue) {
     return windChill = Math.round((13.12 + (0.6215 * temperatureValue) - 11.37 * Math.pow(windValue, 0.16) + (0.3965 * temperatureValue) * Math.pow(windValue, 0.16))*100)/100;
 }
-
